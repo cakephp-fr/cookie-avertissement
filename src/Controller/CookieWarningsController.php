20d23bfec1bffff757b3919ec63e37d15a7bccdc
cake17 <cake17@cake-websites.com>
2015-10-30 13:44:30 +0100

    rename files with new plugin name

---
+++ src/Controller/CookieWarningsController.php
@@ -0,0 +1,18 @@
+<?php
+namespace CookieWarning\Controller;
+
+use Cake\Controller\Controller;
+
+class CookieWarningsController extends Controller
+{
+
+    public function displayInfo()
+    {
+        $this->viewBuilder()->layout(FALSE);
+    }
+
+    public function displayDetail()
+    {
+
+    }
+}
