c144c2abba1107c2b78b01af77f7fc9e2701c697
cake17 <cake17@cake-websites.com>
2015-10-30 16:21:42 +0100

    only 2 routes are needed

--- config/routes.php
+++ config/routes.php
@@ -2,17 +2,12 @@
 use Cake\Routing\Router;
 
 Router::plugin('CookieWarning', function ($routes) {
-    $routes->fallbacks('DashedRoute');
+    $routes->connect(
+        '/cookie-warnings/display-info',
+        ['controller' => 'CookieWarnings', 'action' => 'displayInfo']
+    );
+    $routes->connect(
+        '/cookie-warnings/display-detail',
+        ['controller' => 'CookieWarnings', 'action' => 'displayDetail']
+    );
 });
-
-
-// Router::plugin('CookieWarning', ['path' => '/cookie'], function ($routes) {
-//     $routes->connect(
-//         '/warnings/display-info',
-//         ['controller' => 'CookieWarnings', 'action' => 'displayInfo']
-//     );
-//     $routes->connect(
-//         '/warnings/display-detail',
-//         ['controller' => 'CookieWarnings', 'action' => 'displayDetail']
-//     );
-// });
