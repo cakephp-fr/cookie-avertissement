97be54e8931867582e67ab1d7044b4f9aa5d6235
cake17 <cake17@cake-websites.com>
2015-11-03 16:21:38 +0100

    add TestApp files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,14 +3,31 @@
 use Cake\Cache\Cache;
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
-use Cake\Datasource\ConnectionManager;
-use Cake\I18n\I18n;
+use Cake\Routing\DispatcherFactory;
+use Cake\Routing\Router;
+
+$findRoot = function ($root) {
+    do {
+        $lastRoot = $root;
+        $root = dirname($root);
+        if (is_dir($root . '/vendor/cakephp/cakephp')) {
+            return $root;
+        }
+    } while ($root !== $lastRoot);
+    throw new Exception("Cannot find the root of the application, unable to run tests");
+};
+$root = $findRoot(__FILE__);
+unset($findRoot);
+chdir($root);
+
+require_once $root . DS . 'vendor/cakephp/cakephp/src/basics.php';
+require_once $root . DS . 'vendor/autoload.php';
 
 // Path constants to a few helpful things.
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
 }
-define('ROOT', dirname(__DIR__) . DS);
+define('ROOT', $root . DS);
 define('CAKE_CORE_INCLUDE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
 define('CORE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
 define('CAKE', CORE_PATH . 'src' . DS);
@@ -20,15 +37,15 @@ define('APP_DIR', 'app');
 define('WEBROOT_DIR', 'webroot');
 define('WWW_ROOT', dirname(APP) . DS . 'webroot' . DS);
 define('TMP', sys_get_temp_dir() . DS);
-define('CONFIG', APP . 'config' . DS);
+define('CONFIG', ROOT . 'config' . DS);
 define('CACHE', TMP);
 define('LOGS', TMP);
 
-require_once ROOT . 'vendor/autoload.php';
-require_once CORE_PATH . 'config/bootstrap.php';
-
 date_default_timezone_set('UTC');
 mb_internal_encoding('UTF-8');
+
+Router::defaultRouteClass('DashedRoute');
+
 Configure::write('debug', true);
 Configure::write('App', [
     'namespace' => 'App',
@@ -60,3 +77,6 @@ Cache::config([
     ]
 ]);
 Plugin::load('CookieWarning', ['path' => ROOT, 'bootstrap' => true, 'routes' => true]);
+
+DispatcherFactory::add('Routing');
+DispatcherFactory::add('ControllerFactory');
